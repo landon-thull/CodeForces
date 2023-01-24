2e99cfaa6f12afed6d1d3cb3d22e02302878d5f3
Landon Thull <98464797+landon-thull@users.noreply.github.com>
2023-01-24 01:09:40 -0600

    Initial commit

---
+++ src/Watermelon.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        // init scanner
+        Scanner sc = new Scanner(System.in);
+        // Grab int input
+        int i = sc.nextInt();
+
+        // if i > 2 && even print YES; else print NO
+        if (i % 2 == 0 && i > 2) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
