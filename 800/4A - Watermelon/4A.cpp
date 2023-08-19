6136c14a11b9c3379d13038d7493c8945b6b3876
Landon Thull <landonthull.tx@gmail.com>
2023-08-18 21:25:24 -0500

    use template on 4A - Watermelon

---
+++ 800/4A - Watermelon/4A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace    std;
+
+void solve() {
+  // code here
+  int w;
+  cin >> w;
+
+  if (w > 2 && w % 2 == 0) {
+    cout << "YES\n";
+    return;
+  } 
+
+  cout << "NO\n";
+}
+
+int main() {
+  #ifdef ONLINEJUDGE
+    clock_t tStart = clock();
+    freopen("input.txt","r",stdin); //can need to change file . this one for taking input
+    freopen("output.txt","w",stdout); // this one for output
+  #endif
+
+  // iterator here
+  solve();
+
+  #ifdef ONLINEJUDGE
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC); // this line gives your code runtime
+  #endif
+
+  return 0;
+}
